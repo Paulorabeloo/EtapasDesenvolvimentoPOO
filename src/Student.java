d6d14bfcdaea9f75d140cacbeece4a7400b0b8cd
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-07-20 13:11:09 -0300

    EtapasDesenvolvimentoPOO

---
+++ src/Student.java
@@ -0,0 +1,29 @@
+/*Para ilustrar as etapas de desenvolvimento, orientada a objetos em Java, iremos reproduzir a imagem acima em forma de código, 
+para explicar que primeiro criamos a estrutura correspondente, para assim podermos criá-los com as características e a possibilidade de 
+realização de ações (comportamentos), como se fosse no "mundo real". */
+
+// Criando a classe Student
+// Com todas as características e compartamentos aplicados
+
+public class Student 
+{
+    String name;
+    int age;
+    Color color;
+    Sex sex;
+
+    void eating(Lunch lunch)
+    {
+      //NOSSO CÓDIGO AQUI
+    }
+    void drinking(Juice juice)
+    {
+      //NOSSO CÓDIGO AQUI
+    }
+    void running()
+    {
+      //NOSSO CÓDIGO AQUI
+    }
+}
+
+
