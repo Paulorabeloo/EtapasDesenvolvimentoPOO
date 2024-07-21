c3d4a949671e4cb03a880a0b41091f4367a7b957
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-07-20 22:28:48 -0300

    GettersESetters

--- src/Student.java
+++ src/Student.java
@@ -1,7 +1,3 @@
-/*Para ilustrar as etapas de desenvolvimento, orientada a objetos em Java, iremos reproduzir a imagem acima em forma de código, 
-para explicar que primeiro criamos a estrutura correspondente, para assim podermos criá-los com as características e a possibilidade de 
-realização de ações (comportamentos), como se fosse no "mundo real". */
-
 // Criando a classe Student
 // Com todas as características e compartamentos aplicados
 
