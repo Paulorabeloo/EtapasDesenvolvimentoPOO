51d2b83fc432a2ccbb92222a359834148a45c8f5
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-07-23 22:00:57 -0300

    Encapsulamento

---
+++ src/Encapsulamento/ComputadorPedrinho.java
@@ -0,0 +1,17 @@
+package Encapsulamento;
+
+/*
+ * Simulação do uso da classe MSNMessenger
+ */
+public class ComputadorPedrinho 
+{
+	public static void main(String[] args) 
+    {
+		//abrindo MSN Messenger
+		MSNMessenger msn = new MSNMessenger();
+		
+		msn.enviarMensagem();
+		
+		msn.receberMensagem();
+	}
+}
