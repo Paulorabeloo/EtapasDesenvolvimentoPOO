fcd9b96a04f17fd825e344625d445ae80f9b76b9
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-07-25 06:32:14 -0300

    Heranca

---
+++ src/Heranca/ComputadorPedrinho.java
@@ -0,0 +1,25 @@
+package Heranca;
+
+public class ComputadorPedrinho 
+{
+	public static void main(String[] args) 
+    {
+		System.out.println("MSN");
+		MSNMessenger msn = new MSNMessenger();
+		msn.enviarMensagem();
+		msn.receberMensagem();
+        
+		System.out.println("");
+        System.out.println("Facebook");
+		FacebookMessenger fbm = new FacebookMessenger();
+		fbm.enviarMensagem();
+		fbm.receberMensagem();
+
+		System.out.println("");
+        System.out.println("Telegram");
+		Telegram tlg = new Telegram();
+		tlg.enviarMensagem();
+		tlg.receberMensagem();
+		
+	}
+}
